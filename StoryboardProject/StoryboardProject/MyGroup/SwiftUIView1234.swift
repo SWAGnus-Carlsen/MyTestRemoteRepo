5d7908772c5a1aafd490d11cf9ecbfbdf70902fa
DevFork <DevFork@gmail.com>
2024-08-15 20:36:29 +0300

    added file and group

---
+++ StoryboardProject/StoryboardProject/MyGroup/SwiftUIView1234.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView1234.swift
+//  StoryboardProject
+//
+//  Created by Vitaliy Halai on 15.08.24.
+//
+
+import SwiftUI
+
+struct SwiftUIView1234: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView1234()
+}
